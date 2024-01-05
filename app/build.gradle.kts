55b5ec5b622868c925554faa16839c58e56e9552
BluewyWork <bluewy.s.work@gmail.com>
2024-01-05 19:55:02 +0100

    build: bump dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,7 +54,7 @@ android {
 dependencies {
     implementation("io.coil-kt:coil-compose:2.0.0")
     implementation("androidx.constraintlayout:constraintlayout-compose:1.0.1")  //Distribuición
-    implementation("androidx.navigation:navigation-compose:2.7.5")              //Navigator
+    implementation("androidx.navigation:navigation-compose:2.7.6")              //Navigator
     implementation("androidx.compose.runtime:runtime-livedata:1.5.4")           //Server
     implementation("com.squareup.retrofit2:retrofit:2.9.0")                     //Conexiones API Rest libreria Retrofit
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")               //Conexions API Rest
@@ -63,7 +63,7 @@ dependencies {
     implementation("io.coil-kt:coil:2.5.0")                                     //Url a imagen
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.8.1")
+    implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.10.01"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
