c9ccedbeaa09d1421bf08d1618a6780d77bb62db
BluewyWork <bluewy.s.work@gmail.com>
2024-01-05 19:52:47 +0100

    build: bump targetSdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
     defaultConfig {
         applicationId = "com.intermodular.hotel"
         minSdk = 28
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
