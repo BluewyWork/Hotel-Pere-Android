5329055ae024679d101698393ed1e70ba0a44511
BluewyWork <bluewy.s.work@gmail.com>
2024-01-05 21:12:57 +0100

    refractor & feat: add link to login screen

---
+++ app/src/main/java/com/intermodular/hotel/registro/ui/composables/IconAtras.kt
@@ -0,0 +1,18 @@
+package com.intermodular.hotel.registro.ui.composables
+
+import androidx.compose.foundation.clickable
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.intermodular.hotel.ui.theme.turquesaClaro
+
+@Composable
+fun IconAtras(onClick: () -> Unit) {
+    Icon(
+        Icons.Default.KeyboardArrowLeft, "Atras",
+        tint = turquesaClaro,
+        modifier = Modifier.clickable { onClick() }
+    )
+}
---
+++ app/src/main/java/com/intermodular/hotel/registro/ui/composables/TextLogin.kt
@@ -0,0 +1,15 @@
+package com.intermodular.hotel.registro.ui.composables
+
+import androidx.compose.foundation.clickable
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+
+@Composable
+fun TextLogin(navController: NavController) {
+    androidx.compose.material3.Text(
+        modifier = Modifier
+            .clickable { navController.navigate("login") },
+        text = "Ya tienes cuenta? Haz click aqui para logearte!"
+    )
+}
