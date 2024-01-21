f6b3cbb1fda99befcc1842c13cb80bc7665722eb
AbVicario <abelvicariogarcia@gmail.com>
2024-01-21 13:54:03 +0100

    actualizar dependencias room

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,6 +4,12 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
+    pluginManagement {
+        plugins {
+            kotlin("jvm") version "1.4.3"  // Reemplaza "x.x.x" con la versión de Kotlin que estás utilizando
+            kotlin("plugin.serialization") version "1.4.3"
+        }
+    }
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
